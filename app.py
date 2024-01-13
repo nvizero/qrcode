f35902d61d290fb35d89a1efefecedb2bc790fda
victor.tseng <withvictor@gmail.com>
2024-01-13 22:51:51 +0800

    test

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/qrcode')
-@restrict_hosts(['192.168.1.1','127.0.0.1', '10.0.0.1'])
+@restrict_hosts(['192.168.1.1','127.0.0.1', '10.0.0.1','115.43.117.105'])
 def create_qr_code():
     # Get the text from the query parameter 'code'
     text = request.args.get('code', 'Default Text')
